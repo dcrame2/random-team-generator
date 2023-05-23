4be89a1075d8483f6c82f996ff138ffb4cc14869
Evan N <enew@es99.agency>
2023-05-23 16:37:52 -0500

    add styles and functions

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("hi");
+let buttonClickCount = 0;
+
+function generateTeam() {
+    const nameContainers = document.getElementsByClassName('name-container');
+    const remainingElements = [...nameContainers].filter(container => !container.classList.contains('team-fall'));
+
+    if (remainingElements.length < 2) {
+        const lastRemainingElement = remainingElements[0];
+        const randomTeamNumber = getRandomNumber(1, 4);
+        lastRemainingElement.classList.add(`team-${randomTeamNumber}-fall`);
+        lastRemainingElement.classList.add('team-fall');
+    } else {
+        const randomIndexes = getRandomIndexes(remainingElements.length, 2);
+        const randomTeamNumber = ++buttonClickCount;
+
+        randomIndexes.forEach(index => {
+            const randomElement = remainingElements[index];
+            randomElement.classList.add(`team-${randomTeamNumber}-fall`);
+            randomElement.classList.add('team-fall');
+        });
+    }
+}
+
+function getRandomIndexes(max, count) {
+    const indexes = new Set();
+    while (indexes.size < count) {
+        const randomIndex = Math.floor(Math.random() * max);
+        indexes.add(randomIndex);
+    }
+    return Array.from(indexes);
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
